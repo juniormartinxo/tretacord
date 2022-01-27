727409277ea5ad7da6b88c1b80e17207f8cfb8dd
Junior Martins <amjr.box@gmail.com>
2022-01-27 00:00:47 -0300

    Atualização do ícone de logout

--- resources/icons/index.js
+++
@@ -1,1 +0,0 @@
-export { ReactComponent as LogOut } from './log-out.svg'
--- src/components/Icons/log-out.js
+++ src/components/Icons/log-out.js
@@ -7,9 +7,9 @@ function IconLogOut({ h = 24 }) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       class="feather feather-log-out"
     >
       <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
